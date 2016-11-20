d4b86f1b11e5005a151ebf3dd2927050f0b41f74
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-11-20 16:03:07 -0500

    drag from left hand panel to right

--- inst/htmlwidgets/jointPipeline.js
+++ inst/htmlwidgets/jointPipeline.js
@@ -112,7 +112,52 @@ HTMLWidgets.widget({
           interactive: false
         });
 
+        // Events for drag and drop from first pane to second pane
+        stencilPaper.on('cell:pointerdown', function(cellView, e, x, y) {
+          $('body').append('<div id="flyPaper" style="position:fixed;z-index:100;opacity:.7;pointer-event:none;"></div>');
+          var flyGraph = new joint.dia.Graph,
+            flyPaper = new joint.dia.Paper({
+              el: $('#flyPaper'),
+              model: flyGraph,
+              height: 30,
+              width: 100,
+              interactive: false
+            }),
+            flyShape = cellView.model.clone(),
+            pos = cellView.model.position(),
+            offset = {
+              x: x - pos.x,
+              y: y - pos.y
+            };
 
+          flyShape.position(0, 0);
+          flyGraph.addCell(flyShape);
+          $("#flyPaper").offset({
+            left: e.pageX - offset.x,
+            top: e.pageY - offset.y
+          });
+          $('body').on('mousemove.fly', function(e) {
+            $("#flyPaper").offset({
+              left: e.pageX - offset.x,
+              top: e.pageY - offset.y
+            });
+          });
+          $('body').on('mouseup.fly', function(e) {
+            var x = e.pageX,
+              y = e.pageY,
+              target = paper.$el.offset();
+
+            // Dropped over paper ?
+            if (x > target.left && x < target.left + paper.$el.width() && y > target.top && y < target.top + paper.$el.height()) {
+              var s = flyShape.clone();
+              s.position(x - target.left - offset.x, y - target.top - offset.y);
+              graph.addCell(s);
+            }
+            $('body').off('mousemove.fly').off('mouseup.fly');
+            flyShape.remove();
+            $('#flyPaper').remove();
+          });
+        });
       },
 
       resize: function(width, height) {
