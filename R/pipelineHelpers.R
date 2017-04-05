527ba9d5b30326bf82d9717f67dde9f0a5966679
Harvey Lieberman <harvey.lieberman@gmail.com>
2017-04-05 13:43:58 -0400

    replace nodeOut$message with nodeOut$output to reflect changes in node structure.  Covers previous commit too (incorrectly assigned as a typo)

--- R/pipelineHelpers.R
+++ R/pipelineHelpers.R
@@ -82,10 +82,10 @@ executeNode <- function(fn = NULL, params = list()) {
     list(success = FALSE, output = 'error in running node') ## error caught whilst running node
 #    'error'
   })
-  if (!nodeOut$success) {  ## error reported by node
-    return(list(result = 'error', output = nodeOut$message))
-  } else {  ## Success !!!
-    return(list(result = 'success', output = nodeOut$output))
+  if (!nodeOut$success) {
+    return(list(result = 'error', output = nodeOut$output))  ## error reported by node
+  } else {
+    return(list(result = 'success', output = nodeOut$output))  ## Success !!!
   }
   # if (nodeOut == 'error') {
   #   return(list(result = 'error', output = 'error in running node'))
